0288efdeb6c28e0956e57f2cfec0c9b97c4dbc6f
Alex <Cevalloa@gmail.com>
2026-07-10 12:43:49 -0400

    Initial Commit

---
+++ Pokedex_SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex_SwiftUI
+//
+//  Created by Alex Cevallos on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pokedex_SwiftUITests/Pokedex_SwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  Pokedex_SwiftUITests.swift
+//  Pokedex_SwiftUITests
+//
+//  Created by Alex Cevallos on 7/10/26.
+//
+
+import Testing
+
+struct Pokedex_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

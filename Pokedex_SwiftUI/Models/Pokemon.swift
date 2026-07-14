3cc7c9a9263b75ae538d6edc568f298d3fdd1b34
= <cevalloa@gmail.com>
2026-07-14 17:26:12 -0400

    Adding sprite url and ID

--- Pokedex_SwiftUI/Models/Pokemon.swift
+++ Pokedex_SwiftUI/Models/Pokemon.swift
@@ -5,11 +5,31 @@
 //  Created by Alex Cevallos on 7/10/26.
 //
 
+import Foundation
+
 struct Pokemon: Identifiable, Decodable {
     let name: String
     let url: String
     
     var id: String {
         url
+    }
+    
+    var pokemonID: String? {
+        guard let detailURL = URL(string: url) else {
+            return nil
+        }
+        
+        return detailURL.pathComponents.last
+    }
+    
+    var spriteURL: URL? {
+        guard let pokemonID else {
+            return nil
+        }
+        
+        return URL(
+            string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(pokemonID).png"
+            )
     }
 }
